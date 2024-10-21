4bec76cd0575aeb0fc5fe660819713273f983d6e
El John Bonga <bongaeljohn07@gmail.com>
2024-10-21 15:09:26 +0800

    add nav guard on auth route group for it to be inaccessible to authenticated users

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React from 'react';
+import { redirect } from 'next/navigation';
+import { validateRequest } from '@/lib/auth';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default async function Layout({ children }: LayoutProps) {
+  const { user } = await validateRequest();
+
+  if (user) redirect('/');
+
   return (
     <div className="flex items-start justify-center px-4 pb-6 pt-16 lg:px-8">
       {children}
