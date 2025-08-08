8166a45fd2e12545b13632a4551d65ca5eab289c
El John <bongaeljohn07@gmail.com>
2025-08-08 20:13:18 +0800

    refactor middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,38 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getSessionCookie } from 'better-auth/cookies';
 
+const protectedRoutes = [
+  '/upload-recipe',
+  '/my-recipes/uploads',
+  '/my-recipes/saved',
+  '/my-recipes/liked',
+  '/settings',
+];
+const authRoutes = ['/sign-in', '/sign-up'];
+
 export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isAuthRoute = authRoutes.includes(path);
+
   const sessionCookie = getSessionCookie(request, {
     cookiePrefix: 'savorypoint',
   });
 
-  if (!sessionCookie) {
-    const url = new URL('/sign-in', request.url);
-    url.searchParams.set('redirect', request.nextUrl.pathname);
-    return NextResponse.redirect(url);
+  if (sessionCookie && isAuthRoute) {
+    return NextResponse.redirect(new URL('/', request.nextUrl));
+  }
+
+  if (isProtectedRoute && !sessionCookie) {
+    const redirectURL = new URL('/sign-in', request.nextUrl);
+    redirectURL.searchParams.set('redirect', path);
+    return NextResponse.redirect(redirectURL);
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/upload-recipe'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
 };
