8537ca319e994c68a718a75a6a8f6a46fa52f418
El John <bongaeljohn07@gmail.com>
2025-08-20 14:54:12 +0800

    remove prisma nextjs monorepo workaround plugin

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from 'next';
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 const nextConfig: NextConfig = {
   devIndicators: false,
@@ -11,13 +10,6 @@ const nextConfig: NextConfig = {
         hostname: 'api.dicebear.com',
       },
     ],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-
-    return config;
   },
 };
 
