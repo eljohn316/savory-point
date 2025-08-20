3dabaeb16c8aead0a7e4582d1dcfab130638aa11
El John <bongaeljohn07@gmail.com>
2025-08-20 11:13:53 +0800

    fix attempt at prisma orm error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,14 @@
-import { PrismaClient } from '@/generated/prisma';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
 
+dotenv.config();
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+const prismaInstance = new PrismaClient({ adapter });
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || prismaInstance;
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
